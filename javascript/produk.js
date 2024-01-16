1e7da3d916780a4eed9c60874999805cc22f3b22
Kujih <jonathanix2m@gmail.com>
2024-01-16 15:30:59 +0700

    Updates

--- javascript/produk.js
+++ javascript/produk.js
@@ -1,8 +1,8 @@
 const produkDescription = [
-  "This steak can have seasonal taste",
-  "This steak from the best cut of the beef so you should try it",
-  "This is a product Description 3",
-  "This is a product Description 4",
+  "Seared to a succulent caramelize, our classic ribeye is a symphony of texture and flavor. Juicy marbling dances with a smoky char, delivering a steakhouse experience in every bite. Indulge in pure beefy satisfaction, perfect for those seeking a timeless taste of tradition.",
+  "Experience melt-in-your-mouth luxury with our filet mignon. This lean cut, kissed by fire, boasts an impossibly tender texture and delicate flavor. Savored plain or dressed with a light sauce, it's an elegant choice for celebrating special occasions or treating yourself to a refined dining experience.  ",
+  "For the adventurous palate, our cowboy ribeye takes it up a notch. Seasoned with a fiery blend of spices, it explodes with bold, smoky heat. Charred to perfection, the juicy interior offers a welcome contrast, creating a dynamic dance of flavor on your tongue. Dare to conquer this flame-kissed masterpiece?",
+  "Aromatic herbs and a zesty lemon crust awaken your taste buds in this elevated New York strip. The rich beef mingles with the vibrant freshness of the herbs, creating a complex and satisfying balance. Enjoy a lighter yet equally delicious steak experience, perfect for those seeking a touch of sophistication",
 ];
 
 const menuCards = document.querySelectorAll('.menu-card');
